65a3e2bcf14472edf63a309ea6269e57ab206511
farajnazeer <farajnazeer@localhost>
2019-05-16 15:52:14 +0300

    Implemented Add multiple functions whereever it is needed.

--- resources/htmls/js/custom.js
+++ resources/htmls/js/custom.js
@@ -9,8 +9,22 @@ $(document).ready(function($) {
 	// $('#multiple-checkboxes').multiselect({
  //      includeSelectAllOption: true,
  //    });
-    $('select').selectpicker();
+    $('.selectpicker').selectpicker();
 
     // $('.select-two').select2();
+
+
+    //Add multiple
+    $('body').on( "click", ".add-button", function() {
+    	var this_has_add_content = $(this).parents('.has-add-content');
+    	var this_add_this = this_has_add_content.find('.add-this').html();
+    	var this_add_here = this_has_add_content.find('.add-here');
+    	this_add_here.append(this_add_this);
+    });
+
+    //Delete this
+    $('body').on( "click", ".delete-btn", function() {
+    	$(this).parents('.delete-this').remove();
+    });
 
 });
