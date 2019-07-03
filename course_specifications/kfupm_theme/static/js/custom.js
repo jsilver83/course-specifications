daf6e3628430ebb94d945ae58ebd3d6e1ffa9311
farajnazeer <farajnazeer@localhost>
2019-07-03 16:30:53 +0300

    Implemented Form element's name and id value increment.

--- course_specifications/kfupm_theme/static/js/custom.js
+++ course_specifications/kfupm_theme/static/js/custom.js
@@ -15,11 +15,30 @@ $(document).ready(function($) {
 
 
     //Add multiple
+    // $('body').on( "click", ".add-button", function() {
+    //     var this_has_add_content = $(this).parents('.has-add-content');
+    //     var this_add_this = this_has_add_content.find('.add-this').html();
+    //     var this_add_here = this_has_add_content.find('.add-here');
+    //     this_add_here.append(this_add_this);
+    // });
+
     $('body').on( "click", ".add-button", function() {
         var this_has_add_content = $(this).parents('.has-add-content');
-        var this_add_this = this_has_add_content.find('.add-this').html();
         var this_add_here = this_has_add_content.find('.add-here');
-        this_add_here.append(this_add_this);
+        var this_last_content = this_has_add_content.find('.add-here .delete-this:visible').last().clone();
+        var this_cloned_forms = this_last_content.find("input, textarea");
+        this_cloned_forms.val("");
+        var total_forms_count = this_add_here.find("input[name*='-TOTAL_FORMS']");
+        var current_form_count = parseInt(total_forms_count.val()) + 1;
+        total_forms_count.val(current_form_count);
+        var name_number = this_cloned_forms.attr('name').match(/\d+/);
+        var id_number = this_cloned_forms.attr('id').match(/\d+/);
+        var name_attr = this_cloned_forms.attr('name').replace(id_number, current_form_count);
+        var id_attr = this_cloned_forms.attr('id').replace(id_number, current_form_count);
+        this_cloned_forms.attr('name', name_attr);
+        this_cloned_forms.attr('id', id_attr);
+        //});
+        this_add_here.append(this_last_content);
     });
 
     //Delete this
@@ -49,22 +68,21 @@ $(document).ready(function($) {
         $(this).parents('.clo-sec').find('.clo-count').html(clo_count);
     });
 
-
     //Increment attribute numbers while clicking add button
-    $('body').on( "click", "#nav-lecture .add-button", function(){
-        var total_forms_count = $("input[name*='-TOTAL_FORMS']");
-        var current_form_count = parseInt(total_forms_count.val()) + 1;
-        total_forms_count.val(current_form_count);
-        $('.add-here .delete-this:last-child .has-increment-val').each(function() {
-            $.each(this.attributes, function() {
-                if(this.specified && this.value.indexOf("increment_num") > -1) {
-                    var selected_attr_name = this.name;
-                    var selected_attr_val = this.value.replace('increment_num', '');
-                    this.value = selected_attr_val+current_form_count;
-                    //console.log(this.name, this.value);
-                }
-            });
-        });
-    });
+    // $('body').on( "click", "#nav-lecture .add-button", function(){
+    //     var total_forms_count = $("input[name*='-TOTAL_FORMS']");
+    //     var current_form_count = parseInt(total_forms_count.val()) + 1;
+    //     total_forms_count.val(current_form_count);
+    //     $('.add-here .delete-this:visible').last().find('.has-increment-val').each(function() {
+    //         $.each(this.attributes, function() {
+    //             if(this.specified && this.value.indexOf("increment_num") > -1) {
+    //                 var selected_attr_name = this.name;
+    //                 var selected_attr_val = this.value.replace('increment_num', '');
+    //                 this.value = selected_attr_val+current_form_count;
+    //                 //console.log(this.name, this.value);
+    //             }
+    //         });
+    //     });
+    // });
 
 });
--- resources/htmls/js/custom.js
+++ resources/htmls/js/custom.js
@@ -26,11 +26,18 @@ $(document).ready(function($) {
         var this_has_add_content = $(this).parents('.has-add-content');
         var this_add_here = this_has_add_content.find('.add-here');
         var this_last_content = this_has_add_content.find('.add-here .delete-this:visible').last().clone();
-        //alert(this_last_content);
-        //var new_num = 1;
         var this_cloned_forms = this_last_content.find("input, textarea");
         this_cloned_forms.val("");
-        alert(this_cloned_forms.attr('name'));
+        var total_forms_count = this_add_here.find("input[name*='-TOTAL_FORMS']");
+        var current_form_count = parseInt(total_forms_count.val()) + 1;
+        total_forms_count.val(current_form_count);
+        var name_number = this_cloned_forms.attr('name').match(/\d+/);
+        var id_number = this_cloned_forms.attr('id').match(/\d+/);
+        var name_attr = this_cloned_forms.attr('name').replace(id_number, current_form_count);
+        var id_attr = this_cloned_forms.attr('id').replace(id_number, current_form_count);
+        this_cloned_forms.attr('name', name_attr);
+        this_cloned_forms.attr('id', id_attr);
+        //});
         this_add_here.append(this_last_content);
     });
 
@@ -62,20 +69,20 @@ $(document).ready(function($) {
     });
 
     //Increment attribute numbers while clicking add button
-    $('body').on( "click", "#nav-lecture .add-button", function(){
-        var total_forms_count = $("input[name*='-TOTAL_FORMS']");
-        var current_form_count = parseInt(total_forms_count.val()) + 1;
-        total_forms_count.val(current_form_count);
-        $('.add-here .delete-this:visible').last().find('.has-increment-val').each(function() {
-            $.each(this.attributes, function() {
-                if(this.specified && this.value.indexOf("increment_num") > -1) {
-                    var selected_attr_name = this.name;
-                    var selected_attr_val = this.value.replace('increment_num', '');
-                    this.value = selected_attr_val+current_form_count;
-                    //console.log(this.name, this.value);
-                }
-            });
-        });
-    });
+    // $('body').on( "click", "#nav-lecture .add-button", function(){
+    //     var total_forms_count = $("input[name*='-TOTAL_FORMS']");
+    //     var current_form_count = parseInt(total_forms_count.val()) + 1;
+    //     total_forms_count.val(current_form_count);
+    //     $('.add-here .delete-this:visible').last().find('.has-increment-val').each(function() {
+    //         $.each(this.attributes, function() {
+    //             if(this.specified && this.value.indexOf("increment_num") > -1) {
+    //                 var selected_attr_name = this.name;
+    //                 var selected_attr_val = this.value.replace('increment_num', '');
+    //                 this.value = selected_attr_val+current_form_count;
+    //                 //console.log(this.name, this.value);
+    //             }
+    //         });
+    //     });
+    // });
 
 });
