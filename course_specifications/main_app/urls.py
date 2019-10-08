bc3a52d3617efb8b0cf36a789e7811c3d7b77387
Abdullah Joud <joud@kfupm.edu.sa>
2019-10-08 10:14:27 +0300

    created the first page of the review course functionality

--- course_specifications/main_app/urls.py
+++ course_specifications/main_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('new-course/', NewCourseView.as_view(), name='new_course'),
     path('assign-caretakers/<int:pk>/', AssignCaretakersView.as_view(), name='assign_caretakers'),
     path('update-course/<int:pk>/', UpdateCourseView.as_view(), name='update_course'),
-    path('view-course-release/<int:pk>/', UpdateCourseView.as_view(), name='view_course_release'),
+
     path('course-description/<int:pk>/', course_description, name='course_description'),
     path('course-contents/<int:pk>/', course_contents, name='course_contents'),
     path('assessment-tasks/<int:pk>/', assessment_tasks, name='assessment_tasks'),
@@ -17,7 +17,8 @@ urlpatterns = [
     path('evaluation/<int:pk>/', EvaluationView.as_view(), name='evaluation'),
     path('accreditation-requirements/<int:pk>/', accreditation_requirements, name='accreditation_requirements'),
 
-
+    path('view-course-release/<int:pk>/', ReviewCourseView.as_view(), name='view_course_release'),
     path('review/<int:pk>/', ReviewCourseView.as_view(), name='review_course_release'),
+
     path('new-comment/', CreateCommentView.as_view(), name='create_comment'),
 ]
