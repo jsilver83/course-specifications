1f595428283d679793c97d5b90e9187d11a5e687
Ali Aljowaher <ali.aljowaher@kfupm.edu.sa>
2019-10-13 15:24:43 +0300

    fixed some JS bugs and added limit to CLOs to 7

--- course_specifications/kfupm_theme/static/js/custom.js
+++ course_specifications/kfupm_theme/static/js/custom.js
@@ -1,16 +1,48 @@
-$(document).ready(function($) {
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$(document).ready(function ($) {
+    
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            let csrftoken = getCookie('csrftoken');
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
 
     $('.selectpicker').selectpicker();
 
     function select_two() {
         try {
             $('.select2').not('.add-this .select2').select2();
+        } catch (e) {
         }
-        catch(e){}
     }
+
     select_two();
 
-    $('.add_more').click(function() {
+    
+    $('.add_more').click(function () {
         var this_has_add_content = $(this).parents('.has-add-content');
         var form_idx = this_has_add_content.find("input[name*='-TOTAL_FORMS']").val();
         console.log('Total Forms:', form_idx);
@@ -18,9 +50,22 @@ $(document).ready(function($) {
         this_has_add_content.find('.add-here').append(append_form.replace(/__prefix__/g, form_idx).replace(/__placeholder__/g, form_idx));
         this_has_add_content.find("input[name*='-TOTAL_FORMS']").val(parseInt(form_idx) + 1);
         select_two();
+
+
+         //  list of learning outcomes limit to 7 items
+            var number_of_list_items = $('#listOfLearnOutcome li:visible').length;
+                console.log("total # of list items is: " + number_of_list_items);
+                if(number_of_list_items === 7){
+                    $('#outcomeAddBtn').attr("disabled", true);
+                }else{
+                    $('#outcomeAddBtn').attr("disabled", false);
+                }
     });
 
-    $('body').on( "click", ".add-button", function() {
+
+
+    $('body').on("click", ".add-button", function () {
+
         var this_has_add_content = $(this).parents('.has-add-content');
         var this_add_here = this_has_add_content.find('.add-here');
         var this_last_content = this_has_add_content.find('.add-here .delete-this:visible').last().clone();
@@ -29,34 +74,37 @@ $(document).ready(function($) {
         var total_forms_count = this_add_here.find("input[name*='-TOTAL_FORMS']");
         var current_form_count = parseInt(total_forms_count.val()) + 1;
         total_forms_count.val(current_form_count);
+        console.log("total form count" + total_forms_count);
         var name_number = this_cloned_forms.attr('name').match(/\d+/);
         var id_number = this_cloned_forms.attr('id').match(/\d+/);
-
         var name_attr = this_cloned_forms.attr('name').replace(id_number, current_form_count);
         var id_attr = this_cloned_forms.attr('id').replace(id_number, current_form_count);
-        
         this_cloned_forms.attr('name', name_attr);
         this_cloned_forms.attr('id', id_attr);
 
         //Clone last content with replaced numbers
         this_add_here.append(this_last_content);
 
-        this_last_content.find('.has-increment-val').each(function (){
-            if($(this).attr('data-target')){
+        this_last_content.find('.has-increment-val').each(function () {
+            if ($(this).attr('data-target')) {
                 var data_target_num = $(this).attr('data-target').match(/\d+/);
                 var data_target_attr = $(this).attr('data-target').replace(data_target_num, current_form_count);
                 $(this).attr('data-target', data_target_attr);
+
             }
-            if($(this).attr('id')){
+            if ($(this).attr('id')) {
                 var id_modal_num = $(this).attr('id').match(/\d+/);
                 var id_modal_attr = $(this).attr('id').replace(id_modal_num, current_form_count);
                 $(this).attr('id', id_modal_attr);
             }
-            if($(this).attr('aria-labelledby')){
+            if ($(this).attr('aria-labelledby')) {
                 var aria_labelledby_num = $(this).attr('aria-labelledby').match(/\d+/);
                 var aria_labelledby_attr = $(this).attr('aria-labelledby').replace(aria_labelledby_num, current_form_count);
                 $(this).attr('aria-labelledby', aria_labelledby_attr);
+
             }
+
+
         });
 
         select_two();
@@ -66,11 +114,12 @@ $(document).ready(function($) {
     $('.delete-this, .hours-val').addClass('active');
 
     //Actual Contact hours calculation
-    function total_contact_hrs(){
-        var total_contact_hrs = lec_hrs+lab_hrs+tutorial_hrs+practical_hrs+other_hrs;
-        $('.total_contact_hrs').addClass('text-dark h6 font-weight-bold').html(total_contact_hrs +' hrs');
+    function total_contact_hrs() {
+        var total_contact_hrs = lec_hrs + lab_hrs + tutorial_hrs + practical_hrs + other_hrs;
+        $('.total_contact_hrs').addClass('text-dark h6 font-weight-bold').html(total_contact_hrs + ' hrs');
     }
-    function contact_hrs_calc(){
+
+    function contact_hrs_calc() {
         var lec_hrs = 0;
         var lab_hrs = 0;
         var tutorial_hrs = 0;
@@ -79,90 +128,106 @@ $(document).ready(function($) {
 
         var active_element = $('.hours-val.active');
 
-        active_element.find('.contact_hrs.lec').each(function(){
-            if($(this).val()){
+        active_element.find('.contact_hrs.lec').each(function () {
+            if ($(this).val()) {
                 lec_hrs += parseInt($(this).val());
-                $('.total_lec_contact_hrs').addClass('text-dark h6 font-weight-bold').html(lec_hrs +' hrs');
+                $('.total_lec_contact_hrs').addClass('text-dark h6 font-weight-bold').html(lec_hrs + ' hrs');
             }
         });
-        active_element.find('.contact_hrs.lab').each(function(){
-            if($(this).val()){
+        active_element.find('.contact_hrs.lab').each(function () {
+            if ($(this).val()) {
                 lab_hrs += parseInt($(this).val());
-                $('.total_lab_contact_hrs').addClass('text-dark h6 font-weight-bold').html(lab_hrs +' hrs');
+                $('.total_lab_contact_hrs').addClass('text-dark h6 font-weight-bold').html(lab_hrs + ' hrs');
             }
         });
-        active_element.find('.contact_hrs.tutorial').each(function(){
-            if($(this).val()){
+        active_element.find('.contact_hrs.tutorial').each(function () {
+            if ($(this).val()) {
                 tutorial_hrs += parseInt($(this).val());
-                $('.total_tutorial_contact_hrs').addClass('text-dark h6 font-weight-bold').html(tutorial_hrs +' hrs');
+                $('.total_tutorial_contact_hrs').addClass('text-dark h6 font-weight-bold').html(tutorial_hrs + ' hrs');
             }
         });
-        active_element.find('.contact_hrs.practical').each(function(){
-            if($(this).val()){
+        active_element.find('.contact_hrs.practical').each(function () {
+            if ($(this).val()) {
                 practical_hrs += parseInt($(this).val());
-                $('.total_practical_contact_hrs').addClass('text-dark h6 font-weight-bold').html(practical_hrs +' hrs');
+                $('.total_practical_contact_hrs').addClass('text-dark h6 font-weight-bold').html(practical_hrs + ' hrs');
             }
         });
-        active_element.find('.contact_hrs.other').each(function(){
-            if($(this).val()){
+        active_element.find('.contact_hrs.other').each(function () {
+            if ($(this).val()) {
                 other_hrs += parseInt($(this).val());
-                $('.total_other_contact_hrs').addClass('text-dark h6 font-weight-bold').html(other_hrs +' hrs');
+                $('.total_other_contact_hrs').addClass('text-dark h6 font-weight-bold').html(other_hrs + ' hrs');
             }
         });
         console.log(lec_hrs, lab_hrs, tutorial_hrs, practical_hrs, other_hrs);
-        var total_contact_hrs = lec_hrs+lab_hrs+tutorial_hrs+practical_hrs+other_hrs;
-        $('.total_contact_hrs').addClass('text-dark h6 font-weight-bold').html(total_contact_hrs +' hrs');        
+        var total_contact_hrs = lec_hrs + lab_hrs + tutorial_hrs + practical_hrs + other_hrs;
+        $('.total_contact_hrs').addClass('text-dark h6 font-weight-bold').html(total_contact_hrs + ' hrs');
     }
+
     contact_hrs_calc();
 
-    function self_study_contact_hrs(){
+    function self_study_contact_hrs() {
         var self_study_contact_hrs = 0;
-        $('#self-study .contact_hrs input').each(function(){
-            if($(this).val()){
+        $('#self-study .contact_hrs input').each(function () {
+            if ($(this).val()) {
                 self_study_contact_hrs += parseInt($(this).val());
                 console.log('Self study hrs', self_study_contact_hrs);
                 $('#total_self_study_contact_hrs').html(self_study_contact_hrs);
             }
         });
     }
+
     self_study_contact_hrs();
 
-    $('body').on('keyup', '#self-study .contact_hrs input', function(){
+    $('body').on('keyup', '#self-study .contact_hrs input', function () {
         self_study_contact_hrs();
     });
 
-    $('body').on('keyup', '.form-accordian input.contact_hrs', function(){
+    $('body').on('keyup', '.form-accordian input.contact_hrs', function () {
         contact_hrs_calc($(this));
     });
 
     //Delete this
-    $('body').on( "click", ".delete-btn", function() {
+    $('body').on("click", ".delete-btn", function () {
         var delete_this = $(this).parents('.delete-this');
         delete_this.removeClass('active').hide();
         contact_hrs_calc();
-        if(!delete_this.is(":visible")){
-            $(this).find('input[type=checkbox]').prop( "checked", true );
+        if (!delete_this.is(":visible")) {
+            $(this).find('input[type=checkbox]').prop("checked", true);
+        } else {
+            $(this).find('input[type=checkbox]').prop("checked", false);
         }
-        else{
-            $(this).find('input[type=checkbox]').prop( "checked", false );
-        }
+
+
+        // disable delete button if form has one list item
+        var number_of_list_items = $('#listOfLearnOutcome li:visible').length;
+            if(number_of_list_items < 7){
+                $('#outcomeAddBtn').attr("disabled", false);
+
+            };
+            if(number_of_list_items === 1){
+                $('#outcomeDelBtn').attr("disabled",true);
+            }
+
+
     });
 
     //Delete adding content for submitting form
-    $('body').on( "click", ".delete-adding-content", function() {
+    $('body').on("click", ".delete-adding-content", function () {
         $('.add-this').remove();
+
+
     });
 
-    if($('.alert-popup').is(':visible')){
+    if ($('.alert-popup').is(':visible')) {
         var alert_popup_width = $('.alert-popup').width();
-        $('.alert-popup').css("margin-left",-alert_popup_width/2);
+        $('.alert-popup').css("margin-left", -alert_popup_width / 2);
     }
 
 
     // function clo_counts(){
     // }
 
-    $('body').on( "click", ".update-clo", function() {
+    $('body').on("click", ".update-clo", function () {
         // var clo_count = $(this).parents('.modal-footer').siblings('.modal-body').find('.select2-selection__choice').length;
         // $(this).parents('.clo-sec').find('.add_clo').hide();
         // $(this).parents('.clo-sec').find('.add_clo.added').show();
@@ -178,7 +243,7 @@ $(document).ready(function($) {
         // else{
         // $(this).find('.add_clo.added').show();
         $(this).parents('.modal').siblings('.btn').find('.clo-count').html(added_clo_count);
-            // }
+        // }
         // });
     });
 
@@ -188,11 +253,11 @@ $(document).ready(function($) {
     // });
 
     //Checklist submenu dropdown
-    $('body').on( "click", '.checklist-menu > .has-sub-menu .drop-btn', function() {
+    $('body').on("click", '.checklist-menu > .has-sub-menu .drop-btn', function () {
         $(this).parents('.has-sub-menu').toggleClass('open');
     });
 
-    function touchscroll_height(){
+    function touchscroll_height() {
         var h_window = $(window).outerHeight(true);
         var h_course_title = $('.course-title').outerHeight(true);
         var h_navbar = $('.navbar').outerHeight(true);
@@ -202,95 +267,93 @@ $(document).ready(function($) {
         var h_chariman_view_layout = $('.chariman-view-layout').outerHeight() - 32;
         $('#touchScroller').css('height', h_chariman_view_layout - h_course_title - h_btn_row + "px");
     }
+
     //touchscroll_height();
-    $( window ).resize(function() {
+    $(window).resize(function () {
         //touchscroll_height();
     });
     $('#touchScroller .card-body').css('overflow-y', 'auto');
 
 
     //Completed main menu
-    function menu_completed_check(){
-        $('.checklist-menu .sub-menu').each(function (){
+    function menu_completed_check() {
+        $('.checklist-menu .sub-menu').each(function () {
             if ($(this).find("a.completed").length == $(this).find("a").length) {
                 $(this).parents('li').addClass('completed');
-            }
-            else{
+            } else {
                 $(this).parents('li').removeClass('completed');
             }
         });
     }
 
     //Card ID
-    function card_id(thisObj){
-        if(thisObj.hasClass('comment-btn')){
+    function card_id(thisObj) {
+        if (thisObj.hasClass('comment-btn')) {
             var this_id = thisObj.parents('.card').attr('id');
-            $('.checklist-menu').find('.'+this_id).addClass('completed with-comments');
+            $('.checklist-menu').find('.' + this_id).addClass('completed with-comments');
         }
-        if(thisObj.hasClass('check-btn')){
+        if (thisObj.hasClass('check-btn')) {
             var this_id = thisObj.parents('.card').attr('id');
-            $('.checklist-menu').find('.'+this_id).addClass('completed');
+            $('.checklist-menu').find('.' + this_id).addClass('completed');
         }
-        if(thisObj.hasClass('undo-btn')){
+        if (thisObj.hasClass('undo-btn')) {
             var this_id = thisObj.parents('.card').attr('id');
-            $('.checklist-menu').find('.'+this_id).removeClass('completed with-comments');  
+            $('.checklist-menu').find('.' + this_id).removeClass('completed with-comments');
         }
     }
 
     //Action button
-    $('body').on( "click", '.comment-btn', function(){
+    $('body').on("click", '.comment-btn', function () {
         var this_parent = $(this).parents('.action-section');
         this_parent.parents('.card').addClass('commented');
         card_id($(this));
         menu_completed_check();
     });
-    $('body').on( "click", '.check-btn', function(){
+    $('body').on("click", '.check-btn', function () {
         var this_parent = $(this).parents('.action-section');
         this_parent.parents('.card').addClass('checked');
         card_id($(this));
         menu_completed_check();
     });
-    $('body').on( "click", '.undo-btn', function(){
+    $('body').on("click", '.undo-btn', function () {
         $('.action-section').find('input[type="checkbox"]').prop('checked', false);
         $(this).parents('.card').removeClass('checked commented');
         card_id($(this));
-        if($('.check-all').hasClass('active')){
+        if ($('.check-all').hasClass('active')) {
             $('.check-all').removeClass('active');
         }
         menu_completed_check();
     });
 
 
-    $('body').on( "click", '.check-all', function(){
-        if($(this).hasClass('active')){
+    $('body').on("click", '.check-all', function () {
+        if ($(this).hasClass('active')) {
             $('.action-section').find('input[type="checkbox"]').prop('checked', false);
             $('#touchScroller .card').removeClass('commented checked');
-            $('.checklist-menu').find('a').removeClass('completed with-comments'); 
+            $('.checklist-menu').find('a').removeClass('completed with-comments');
             $(this).removeClass('active');
-        }
-        else{
+        } else {
             $('.action-section').find('input[type="checkbox"]').prop('checked', false);
             $('.action-section .check-btn input[type="checkbox"]').prop('checked', true);
             $('#touchScroller .card').removeClass('commented').addClass('checked');
             $(this).addClass('active');
-            $('.checklist-menu .active').find('a').addClass('completed').removeClass('with-comments');   
+            $('.checklist-menu .active').find('a').addClass('completed').removeClass('with-comments');
         }
         menu_completed_check();
     });
 
-    $('.action-section input[type="checkbox"]').each(function (){
-        if($(this).prop('checked')){
+    $('.action-section input[type="checkbox"]').each(function () {
+        if ($(this).prop('checked')) {
             $(this).parents('.card').removeClass('commented checked');
             var this_p_btn = $(this).parent('.btn');
-            if(this_p_btn.hasClass('comment-btn')){
+            if (this_p_btn.hasClass('comment-btn')) {
                 $(this).parents('.card').addClass('commented');
                 var this_id = this_p_btn.parents('.card').attr('id');
-                $('.checklist-menu').find('.'+this_id).addClass('completed with-comments');                
-            }
-            else if(this_p_btn.hasClass('check-btn')){
+                $('.checklist-menu').find('.' + this_id).addClass('completed with-comments');
+            } else if (this_p_btn.hasClass('check-btn')) {
                 $(this).parents('.card').addClass('checked');
                 var this_id = this_p_btn.parents('.card').attr('id');
-                $('.checklist-menu').find('.'+this_id).addClass('completed');                
+                $('.checklist-menu').find('.' + this_id).addClass('completed');
             }
         }
     });
@@ -298,7 +361,7 @@ $(document).ready(function($) {
     //menu_completed_check();
 
     //Chat view scroll to bottom
-    if($('#chat-scroll').is(':visible')){
+    if ($('#chat-scroll').is(':visible')) {
         $('#chat-scroll').animate({
             scrollTop: $('#chat-scroll').get(0).scrollHeight
         }, 500);
