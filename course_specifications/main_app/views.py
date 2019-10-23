38aa821b26cc658ba9419e39953736ad457b704c
Abdullah Joud <joud@kfupm.edu.sa>
2019-10-23 10:28:25 +0300

    added pagination nav in the courses list

--- course_specifications/main_app/views.py
+++ course_specifications/main_app/views.py
@@ -29,6 +29,7 @@ class CoursesListView(AllowedUserTypesMixin, ListView):
     template_name = 'main_app/courses_list.html'
     context_object_name = 'courses'
     allowed_user_types = '__all__'
+    paginate_by = 10
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(object_list=object_list, **kwargs)
