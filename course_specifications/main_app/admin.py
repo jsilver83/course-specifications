a7b853ad2e182d2d9fba87de513263e4f33475d4
Abdullah Joud <joud@kfupm.edu.sa>
2019-11-05 15:06:01 +0300

    added not_to_be_taken_with_courses field to the course model

--- course_specifications/main_app/admin.py
+++ course_specifications/main_app/admin.py
@@ -38,7 +38,8 @@ class CourseAdmin(SimpleHistoryAdmin):
 class CourseReleaseAdmin(admin.ModelAdmin):
     list_display = ('version', 'course', )
     search_fields = ('course__program_code', 'course__number', 'course__title', )
-    readonly_fields = ('course', 'prerequisite_courses', 'corequisite_courses', 'learning_objectives',
+    readonly_fields = ('course', 'prerequisite_courses', 'corequisite_courses', 'not_to_be_taken_with_courses',
+                       'learning_objectives',
                        'learning_outcomes', 'topics', 'assessment_tasks', 'facilities_required', )
 
 
